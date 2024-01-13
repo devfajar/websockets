4ed584d73da2629d5e48a9f15f37758942e97809
devfajar <fajartrengginas@gmail.com>
2024-01-13 14:26:45 +0700

    Adding: Backend Function For Join Room

--- ws-2/server/index.js
+++ ws-2/server/index.js
@@ -35,7 +35,45 @@ io.on('connection', socket => {
     console.log(`User ${socket.id} connected`);
 
     // upon connection - only to user
-    socket.emit('message', "Welcome to the App");
+    socket.emit('message', buildMsg(ADMIN,  "Welcome To The Chat App !"));
+
+    //
+    socket.on('enterRoom', ({ name, room }) => {
+        // leaves previous room
+        const prevRoom = getUser(socket.id)?.room;
+
+        if (prevRoom) {
+            socket.leave(prevRoom);
+            io.to(prevRoom).emit('message', buildMsg(ADMIN, `${name} has left the room`));
+        }
+
+        const user = activateUser(socket.id, name, room);
+        // Cannot update previous room users list until after the state update in activate user
+        if (prevRoom) {
+            io.to(prevRoom).emit('userList', {
+                users: getUsersInRoom(prevRoom)
+            });
+        }
+
+        // Join Room
+        socket.join(user.room);
+
+        // to user who joined
+        socket.emit('message', buildMsg(ADMIN, `You have joined the ${user.room} chat room`));
+
+        // To everyone else
+        socket.broadcast.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} has joined the room`));
+
+        // Update user list for room
+        io.to(user.room).emit('userList', {
+            users: getUsersInRoom(user.room)
+        });
+
+        // Update rooms list for everyone
+        io.emit('roomList', {
+            rooms: getAllActiveRooms()
+        });
+    });
 
     // upon connection - do to the all users
     socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`);
@@ -56,3 +94,44 @@ io.on('connection', socket => {
         socket.broadcast.emit('activity', name)
     });
 });
+
+function buildMsg(name, txt) {
+    return {
+        name,
+        text,
+        time: new Intl.DateTimeFormat('default', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric', 
+        }).format(new Date()) ,
+    }
+}
+
+
+// User Function
+function activateUser(id, name, room) {
+    const user = { id, name, room}
+    UsersState.setUsers([
+        ...UsersState.users.filter(user => user.id !== id),
+        user
+    ]);
+    return user
+}
+
+function userLeavesApp(id) {
+    UsersState.setUsers(
+        UsersState.users.filter(user => user.id !== id)
+    )
+}
+
+function getUser(id) {
+    return UsersState.users.find(user => user.id === id)
+}
+
+function getUsersInRoom(room) {
+    return UsersState.users.filter(user => users.room === room)
+}
+
+function getAllActiveRooms() {
+    return Array.from(new Set(UsersState.users.map(user => user.room)))
+}
